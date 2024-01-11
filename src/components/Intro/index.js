2d8f38806c6440303a732f8b443b6d25ddf66d0a
Sweet <msweet1@partners.org>
2024-01-11 15:50:28 -0500

    set up basic structure for site

---
+++ src/components/Intro/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Intro() {
+    return (
+        <h1>7 South Sandwich Company</h1>
+    )
+}
+
+export default Intro
---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>About</li>
+        </ul>
+    )
+}
+
+export default Nav
